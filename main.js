e4c0618637028e93a288b6b67a6478b27727a47c
cadcad302 <dolbeyca@gmail.com>
2024-05-17 12:59:09 -0400

    mega bug fix

--- main.js
+++ main.js
@@ -4,4 +4,55 @@ document.addEventListener("DOMContentLoaded", function() {
       window.location.href = window.location.origin + '/';
     }
   });
-  
+
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+  //Version
+  const versionHTML = document.getElementById("Version")
+  fetch("version.JSON")
+  .then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+})
+.then(data => {
+    versionHTML.textContent = versionHTML.textContent + data.version;
+})
+.catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+});
+})
+//Config Settings
+document.addEventListener("DOMContentLoaded", () =>{
+  //alert("cadcad302 trying not to completely copy the css of nativegames (impossible edition)")
+
+  fetch("config.JSON")
+  .then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+})
+.then(data => {
+    let configEasingTime = data.defTransition;
+    let defaultFont = data.standardFont;
+    let root = document.querySelector(':root');
+    let rootStyles = getComputedStyle(root);
+    // adding transition
+    root.style.setProperty('--transition-default', configEasingTime);
+    // adding fonts
+    let cssVarFont = rootStyles.getPropertyValue('--standard-font');
+    root.style.setProperty('--standard-font', defaultFont);
+
+
+     
+})
+.catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+});
+
+
+})
