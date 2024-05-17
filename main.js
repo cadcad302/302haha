7a9265d51b246c8e26caddb0eb5880157d0dbb6d
cadcad302 <dolbeyca@gmail.com>
2024-05-17 14:42:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () =>{
   fetch("version.JSON")
   .then(response => {
     if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
+        throw new Error('Could not fetch specific resource' + response.statusText);
     }
     return response.json();
 })
@@ -22,37 +22,30 @@ document.addEventListener("DOMContentLoaded", () =>{
     versionHTML.textContent = versionHTML.textContent + data.version;
 })
 .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
+    console.error(error);
 });
 })
 //Config Settings
 document.addEventListener("DOMContentLoaded", () =>{
   //alert("cadcad302 trying not to completely copy the css of nativegames (impossible edition)")
-
-  fetch("config.JSON")
-  .then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-    }
+  fetch("config.json")
+    .then(response => {
+      if(!response.ok) {
+        throw new Error("Could not fetch specific resource" + response.statusText)
+      }
     return response.json();
-})
-.then(data => {
-    let configEasingTime = data.defTransition;
-    let defaultFont = data.standardFont;
-    let root = document.querySelector(':root');
-    let rootStyles = getComputedStyle(root);
-    // adding transition
-    root.style.setProperty('--transition-default', configEasingTime);
-    // adding fonts
-    let cssVarFont = rootStyles.getPropertyValue('--standard-font');
-    root.style.setProperty('--standard-font', defaultFont);
-
-
-     
-})
-.catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-});
-
-
+    })
+      
+    .then(data => {
+      let configEasingTime = data.defTransition;
+      let defaultFont = data.standardFont;
+      let root = document.querySelector(':root');
+      let rootStyles = getComputedStyle(root);
+      // adding transition
+      root.style.setProperty('--transition-default', configEasingTime);
+      // adding fonts
+      let cssVarFont = rootStyles.getPropertyValue('--standard-font');
+      root.style.setProperty('--standard-font', defaultFont);
+    })
+    .catch(error => console.error(error));
 })
